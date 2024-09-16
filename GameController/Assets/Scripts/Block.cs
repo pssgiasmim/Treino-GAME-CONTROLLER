5d806a13e0324f882f82c25a8eb3088762ec65f3
pssgiasmim <iasmimpassing@gmail.com>
2024-09-16 15:00:23 -0300

    Scripts

--- GameController/Assets/Scripts/Block.cs
+++ GameController/Assets/Scripts/Block.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer spriteRenderer; ////Variavel do tipo SpriteRenderer
+
+    
+
+    //Método responsavel por trocar a sprite
+    private void Start()
     {
-        
+        // Obtém o componente SpriteRenderer
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+
+        // A alteração da cor do bloco vai ocorrer em ChangeColor();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //Método que verifica se o bloco foi clicado
+    private void OnMouseDown() //O OnMouseDown já verifica automaticamente se o elemento já foi clicado
     {
-        
+        ChangeColor(); // Muda a cor do bloco quando ele for clicado
     }
+
+
+    //Método para trocar a cor do bloco aleatóriamente
+    private void ChangeColor()
+    {
+        s = new Color(1,1,1);//SpriteRenderer.color = new Color(repetir o random. range)
+    }
+
 }
