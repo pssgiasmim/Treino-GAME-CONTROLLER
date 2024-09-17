0075d16d03bb8ba48c8be0066309a12a549b44ac
pssgiasmim <iasmimpassing@gmail.com>
2024-09-17 11:39:15 -0300

    Script do GameController

---
+++ GameController/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject blockPrefab; //prefab do bloco
+
+    public int linha = 5; //quantidade de linhas na matriz
+
+    public int coluna = 5; //quantidade de colunas na matriz
+
+    public float spacing = 1.1f; //tamanho do espaçamento entre blocos
+
+    private GameObject[,] grid; //variável da matriz 2D que armazena os blocos
+
+    
+    //Método que inicializa a matriz com a quantidade de linhas e colunas
+    private void Start()
+    {
+        //inicializa a matriz com a quantidade de linhas e colunas
+        grid = new GameObject[linha, coluna];
+
+        //chamando o método de criar o grid para criar o grid(matriz)
+        CreateGrid();
+
+    }
+        
+    //Método para criar a matriz
+    private void CreateGrid()
+    {
+        //loop que cria a matriz. Wsse mesmo loop cálcula a posição de cada boloco e armazena
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid.Length; j++)
+            {
+                //variável que armazena as posições dos blocos
+                Vector2 posicao = new Vector2( i, j); //a posição em linha e coluna, é armazenado na variável posicao
+
+                GameObject bloco=Instantiate(blockPrefab, posicao, Quaternion.identity);
+
+                grid[i , j] = bloco;
+
+
+            }
+        }
+    }
+
+   
+
+
+}
